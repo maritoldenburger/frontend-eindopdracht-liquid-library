7505c1c4afb7e4a3d32ace83daa0a3f669e4fb74
moldenburger <maritoldenburger@hotmail.com>
2025-04-07 17:03:58 +0200

    Mappen en bestanden voor pagina's en componenten aangemaakt, afbeelding en logo toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import RecipeDetail from "./components/RecipeDetail";
+import recepten from "./constants/recipes.json"; // JSON importeren
+import "./styles.css";
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<RecipeList recepten={recepten} />} />
+                <Route path="/recept/:id" element={<RecipeDetail recepten={recepten} />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
