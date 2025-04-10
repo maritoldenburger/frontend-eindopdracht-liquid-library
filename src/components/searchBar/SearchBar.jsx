d6a2b655f26020fb2d2f6e3d4958a35dbf5d33fb
moldenburger <maritoldenburger@hotmail.com>
2025-04-10 18:59:39 +0200

    CSS zoekbalk afgemaakt

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./SearchBar.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar({placeholder, value, onChange, onSubmit, variant}) {
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                type="text"
-                name="searchbar"
-                placeholder={placeholder}
-                value={value} // zet je op query
-                onChange={onChange} // zet je op {(e) => setQuery(e.target.value)}
-                className={`searchbar ${variant}`}
-            />
+        <form onSubmit={onSubmit} className="searchbar-form">
+            <span className="searchbar-icon-wrapper">
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="searchbar-icon"/>
+            </span>
+                <input
+                    type="text"
+                    name="searchbar"
+                    placeholder={placeholder}
+                    value={value} // zet je op query
+                    onChange={onChange} // zet je op {(e) => setQuery(e.target.value)}
+                    className={`searchbar ${variant}`}
+                />
         </form>
     );
 }
